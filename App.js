85263c04cc5375b6e9bdf8b3ba13349d88771fb7
Adriane <adriane.desenvolvimento@gmail.com>
2023-04-12 18:26:59 -0300

    Testando

--- App.js
+++ App.js
@@ -1,40 +1,21 @@
-import React from "react";
-import { TextInput, View } from "react-native";
-import { StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import Navigator from "./routes/homeStack";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
 
 export default function App() {
-  return (
-    <View style={styles.container}> 
-      <Text style={styles.descricao}> Cadastro </Text>
-      <TextInput style={styles.inputs} > Email</TextInput>
-      <TextInput style={styles.inputs} secureTextEntry={true} value="Senha">
-        {"*"}
-        Senha
-      </TextInput>
-      <Text style={styles.botao_login}> Efetuar Login </Text>
-    </View>
-  );
-}
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",//plano de fundo
-    justifyContent: "center",//formatando posição do objeto na tela->centralizado
-    flex: 1,
-    alignItems: "center" //alinhamento dos itens-> centralizado
-  },
-  descricao: {
-    fontFamily: 'normal', //Fonte do texto
-    fontSize: 20, // tamanho da fonte
-    color: "blue" // cor da fonte
-  },
-  inputs: {
-    fontFamily: 'normal', // fonte do texto
-    fontSize: 10, // tamanho da fonte
-    color: 'red' // cor da fonte
-  },
-  botao_login:{
-    borderRadius: 15, // tamanho da fonte
-    color: 'purple', // cor da fonte
-    backgroundColor: 'gray' //cor do plano de fundo
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return <Navigator />;
+  } else {
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+    );
   }
-});
+}
